5b1ee3851288f9698ce746e13f91932c756b0e3f
Michael Redman <michael.redman.4@gmail.com>
2016-08-06 16:14:21 +0000

    Added logic for multiple separate chat rooms in same database

--- ws_server/server.js
+++ ws_server/server.js
@@ -7,20 +7,21 @@ const WebSocketServer = require('ws').Server;
 const setuidgid=require('../share/setuidgid.js');
 const site_conf = require('./site_conf.js');
 
-var db, wss, n_sockets=0, sockets=[], users=[];
+var db, wss, n_sockets=0, sockets=[], users=[], rooms=[];
 
 function authenticate(db,session_key,socket,success_callback,error_callback){
 	db.query(
-		'select sessions.user as user from sessions where key=$1::text',
+		'select sessions.user as user, sessions.room as room from sessions where key=$1::text',
 		[session_key],
 		function(err,result){
 			if	(err)
 				{	console.error(new Date().toISOString() + ' ' + err);
 					error_callback(); }
-				else	if	(result.rows.length>0 && typeof result.rows[0].user != undefined)
+				else	if	(result && result.rows.length>0 && typeof result.rows[0].user != undefined)
 						{	console.error(new Date().toISOString() + ' In authenticate before add: users.length=' + users.length);
 							sockets[n_sockets]=socket;
 							users[n_sockets]=result.rows[0].user;
+							rooms[n_sockets]=result.rows[0].room;
 							n_sockets++;
 							console.error(new Date().toISOString() + ' In authenticate after add: users.length=' + users.length);
 							success_callback(); }
@@ -33,22 +34,26 @@ function delete_from_array(ar,val){
 	var index = ar.indexOf(val);
 	if (index>-1) ar.splice(index,1); }
 
-function broadcast(time,user,message){
-	wss.clients.forEach(function each(client){ if (client.readyState==client.OPEN) client.send(JSON.stringify({
-		time: time,
-		user: user,
-		message: message})); }); }
+function broadcast(room,time,user,message){
+	for	(var i=0; i<n_sockets; i++)
+		{	if (rooms[i]!=room) continue;
+			var client=sockets[i];
+			if (client.readyState==client.OPEN) client.send(JSON.stringify({
+				time: time,
+				user: user,
+				message: message})); }}
 
 function db_notify_handler(msg){
 	console.error(new Date().toISOString() +' Received notify, message ' + msg.payload);
 	db.query(
-		'select * from chat_log where message_id=$1::integer',
+		'select * from log where message_id=$1::integer',
 		[msg.payload],
 		function(err,result){
 			if	(err)
 				{	console.error(new Date().toISOString() + ' ' + err);
 					return; }
 			broadcast(
+				result.rows[0].room,
 				result.rows[0].time,
 				result.rows[0].user,
 				result.rows[0].message); }); }
@@ -97,7 +102,9 @@ module.exports = {
 						ws.on('close',(function(ws,user){ return function(code,message){
 							console.error(new Date().toISOString() + ' socket closed ('+user+'): '+code+' ' + message);
 							console.error(new Date().toISOString() + ' Sockets array length before close: '+sockets.length);
-							delete_from_array(users,users[sockets.indexOf(ws)]);
+							var index=sockets.indexOf(ws);
+							delete_from_array(users,users[index]);
+							delete_from_array(rooms,rooms[index]);
 							delete_from_array(sockets,ws);
 							n_sockets--;
 							console.error(new Date().toISOString() + ' Sockets array length after close: '+sockets.length);
@@ -106,18 +113,27 @@ module.exports = {
 						ws.on('message',(function(user){ return function(message){
 							var json=JSON.parse(message);
 							console.error(new Date().toISOString() + ' received ('+user+'): ' + json.message);
-							db.query('select new_message(now(),$1::text,$2::text)',[users[sockets.indexOf(ws)],json.message],function(err,result){
-								if	(err)
-									{	console.error(new Date().toISOString()+' '+err);
-										ws.close(); } });
+							db.query(
+								'select new_message($1::integer,now(),$2::text,$3::text)',
+								[	rooms[sockets.indexOf(ws)],
+									users[sockets.indexOf(ws)],
+									json.message],
+								function(err,result){
+									if	(err)
+										{	console.error(new Date().toISOString()+' '+err);
+											ws.close(); } });
 							}})(users[sockets.indexOf(ws)]));
-						db.query("select * from chat_log order by time",null,function(err,result){
-							if	(err) {	console.error(new Date().toISOString() + ' ' + error); }
-							for	(var i=0; i<result.rows.length; i++)
-								if (ws.readyState==ws.OPEN) ws.send(JSON.stringify({
-									time: result.rows[i].time,
-									user: result.rows[i].user,
-									message: result.rows[i].message })); });
+						db.query(
+							"select * from log where room=$1::integer order by time",
+							[rooms[sockets.indexOf(ws)]],
+							function(err,result){
+								if (err) console.error(new Date().toISOString() + ' ' + err);
+								if (!result) return;
+								for	(var i=0; i<result.rows.length; i++)
+									if (ws.readyState==ws.OPEN) ws.send(JSON.stringify({
+										time: result.rows[i].time,
+										user: result.rows[i].user,
+										message: result.rows[i].message })); });
 						if	(ws.readyState!=ws.CLOSING && ws.readyState!=ws.CLOSED)
 							ws.send(JSON.stringify({ authenticated: true }));
 						console.error(new Date().toISOString() + ' ' + users[sockets.indexOf(ws)]+' connected.');
