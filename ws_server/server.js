9d5d5c32b3672990bb884009431a32deb89ef0c5
Michael Redman <michael.redman.4@gmail.com>
2016-08-06 05:06:24 +0000

    Added logic to listen & post to PG notify service, to allow multiple websocket server processes to distribute each other's messages

--- ws_server/server.js
+++ ws_server/server.js
@@ -7,7 +7,7 @@ const WebSocketServer = require('ws').Server;
 const setuidgid=require('../share/setuidgid.js');
 const site_conf = require('./site_conf.js');
 
-var n_sockets=0, sockets=[], users=[];
+var db, wss, n_sockets=0, sockets=[], users=[];
 
 function authenticate(db,session_key,socket,success_callback,error_callback){
 	db.query(
@@ -15,7 +15,7 @@ function authenticate(db,session_key,socket,success_callback,error_callback){
 		[session_key],
 		function(err,result){
 			if	(err)
-				{	console.error(new Date.toISOString() + ' ' + error);
+				{	console.error(new Date().toISOString() + ' ' + err);
 					error_callback(); }
 				else	if	(result.rows.length>0 && typeof result.rows[0].user != undefined)
 						{	console.error(new Date().toISOString() + ' In authenticate before add: users.length=' + users.length);
@@ -33,16 +33,39 @@ function delete_from_array(ar,val){
 	var index = ar.indexOf(val);
 	if (index>-1) ar.splice(index,1); }
 
+function broadcast(time,user,message){
+	wss.clients.forEach(function each(client){ if (client.readyState==client.OPEN) client.send(JSON.stringify({
+		time: time,
+		user: user,
+		message: message})); }); }
+
+function db_notify_handler(msg){
+	console.error(new Date().toISOString() +' Received notify, message ' + msg.payload);
+	db.query(
+		'select * from chat_log where message_id=$1::integer',
+		[msg.payload],
+		function(err,result){
+			if	(err)
+				{	console.error(new Date().toISOString() + ' ' + err);
+					return; }
+			broadcast(
+				result.rows[0].time,
+				result.rows[0].user,
+				result.rows[0].message); }); }
+
 module.exports = {
 	init: function(){
-		const db  = new pg.Client(site_conf.db_url);
+		db  = new pg.Client(site_conf.db_url);
 		db.connect(function(err){
 			if	(err)
 				{	console.error(new Date().toISOString() + ' ' + err);
-					server.close();
 					process.exit(1); }
+			db.on('notification',db_notify_handler);
+			db.query('listen nodejschat',function(err,result){
+				if	(err)
+					{	console.error(new Date().toISOString() + ' ' + err);
+						process.exit(1); }});
 			var _http;
-
 			if	(site_conf.cert)
 				{	const credentials={key: site_conf.key, cert: site_conf.cert, ca: site_conf.ca};
 					_http = https.createServer(credentials); }
@@ -55,8 +78,7 @@ module.exports = {
 						process.on('SIGUSR2',function(){
 							process.kill(process.pid,'SIGHUP'); });
 						console.error(new Date().toISOString()+" worker process " + process.pid + " listening https on port " + site_conf.port); }
-
-			var wss=new WebSocketServer({server: _http});
+			wss=new WebSocketServer({server: _http});
 			setuidgid(site_conf.user,site_conf.group);
 			wss.on('error', function(error){
 				console.error(new Date().toISOString() + ' ' + error); });
@@ -84,17 +106,13 @@ module.exports = {
 						ws.on('message',(function(user){ return function(message){
 							var json=JSON.parse(message);
 							console.error(new Date().toISOString() + ' received ('+user+'): ' + json.message);
-							wss.clients.forEach(function each(client){ if (client.readyState==client.OPEN) client.send(JSON.stringify({
-								time: new Date().toISOString(),
-								user: user,
-								message: json.message})); });
-							db.query('insert into chat_log values(now(),$1::text,$2::text)',[users[sockets.indexOf(ws)],json.message],function(err,result){
+							db.query('select new_message(now(),$1::text,$2::text)',[users[sockets.indexOf(ws)],json.message],function(err,result){
 								if	(err)
 									{	console.error(new Date().toISOString()+' '+err);
 										ws.close(); } });
 							}})(users[sockets.indexOf(ws)]));
 						db.query("select * from chat_log order by time",null,function(err,result){
-							if	(err) {	console.error(new Date.toISOString() + ' ' + error); }
+							if	(err) {	console.error(new Date().toISOString() + ' ' + error); }
 							for	(var i=0; i<result.rows.length; i++)
 								if (ws.readyState==ws.OPEN) ws.send(JSON.stringify({
 									time: result.rows[i].time,
